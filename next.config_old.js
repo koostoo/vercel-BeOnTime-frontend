363fcef4ddf20a18983622b6cfbdecdf5d756760
koostoo <yves.coustou@gmail.com>
2022-11-11 18:03:18 +0100

    Config

--- next.config_old.js
+++ next.config_old.js
@@ -1,7 +1,10 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
     backend: "https://vercel-be-on-time-backend.vercel.app",
   },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
